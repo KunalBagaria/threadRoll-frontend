9e1dfafbab9751999ffd7720f6d36aa1e6343284
Kunal Bagaria <kb24x7@gmail.com>
2021-07-23 12:16:11 +0530

    finally

--- components/Meta.js
+++ components/Meta.js
@@ -1,6 +1,8 @@
+import Head from 'next/head'
+
 export const Meta = ({ title, arTitle, description, image }) => (
     <>
-        <head>
+        <Head>
             <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
             <meta charSet="utf-8"></meta>
             <meta name="description" content={description}></meta>
@@ -9,8 +11,14 @@ export const Meta = ({ title, arTitle, description, image }) => (
 
             {/* Twitter */}
             <meta name="twitter:card" content="summary" key="twcard" />
+            <meta property="twitter:card" content="summary_large_image" />
+            <meta property="twitter:url" content="https://threadroll.app/" />
+            <meta property="twitter:title" content={arTitle ? arTitle : title} />
+            <meta property="twitter:description" content={description} />
+            <meta property="twitter:image" content="https://i.imgur.com/O6DPyTR.png" />
 
             {/* Open Graph */}
+            <meta property="og:type" content="website" />
             <meta property="og:url" content={'https://threadroll.app'} key="ogurl" />
             <meta property="og:image" content={image ? image : 'https://i.imgur.com/O6DPyTR.png'} key="ogimage" />
             <meta property="og:site_name" content={'threadRoll'} key="ogsitename" />
@@ -18,6 +26,6 @@ export const Meta = ({ title, arTitle, description, image }) => (
             <meta property="og:description" content={description} key="ogdesc" /> 
 
             <link rel="icon" href="../favicon.ico" />
-        </head>
+        </Head>
     </>
 )
